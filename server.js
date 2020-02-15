8c8904ed2ca7da93fdd5d673ed591853add8dac8
smartsuhani <suhanishah228@gmail.com>
2020-02-15 14:51:28 +0530

    Added Heroku configuration

---
+++ server.js
@@ -0,0 +1,17 @@
+
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist'));
+
+app.get('/*', function(req,res) {
+  console.log(__dirname);
+  res.sendFile(path.join(__dirname,'/dist/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 5000);
